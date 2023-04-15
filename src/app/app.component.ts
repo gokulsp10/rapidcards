5d3e1a6023af68e27d35605484c14e6761e91932
Gokul <gokulsp10@gmail.com>
2023-04-15 21:16:29 +0530

    Task Reminder code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rapidCards';
+  name: string;
+  reminder: Date;
+  tasks: { name: string, reminder: Date, isExpired: boolean }[] = [];
+
+  addTaskVisible = false;
+  viewTaskVisible = true;
+  deleteTaskVisible = false;
+
+  showAddTask() {
+    this.addTaskVisible = true;
+    this.viewTaskVisible = false;
+    this.deleteTaskVisible = false;
+  }
+
+  showViewTask() {
+    this.addTaskVisible = false;
+    this.viewTaskVisible = true;
+    this.deleteTaskVisible = false;
+  }
+
+  showDeleteTask() {
+    this.addTaskVisible = false;
+    this.viewTaskVisible = false;
+    this.deleteTaskVisible = true;
+  }
+
+  addTask() {
+    const newTask = {
+      name: this.name,
+      reminder: new Date(this.reminder),
+      isExpired: false
+    };
+
+    this.tasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+    this.name = '';
+    this.reminder = null;
+    this.showViewTask();
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  deleteAllTasks() {
+    this.tasks = [];
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.showViewTask();
+  }
+
+  ngOnInit() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+
+    setInterval(() => {
+      for (let task of this.tasks) {
+        if (new Date() > new Date(task.reminder)) {
+          task.isExpired = true;
+        }
+      }
+    }, 1000);
+  }
 }
